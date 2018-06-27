87e1b5c087cfff003086ef5748cce1f147ca4566
Josh Gwinn <jdgwinn911@gmail.com>
2018-06-27 16:55:58 -0400

    first fix

--- method_tests.rb
+++ method_tests.rb
@@ -3,7 +3,7 @@ require_relative "methods.rb"
 
 class MethodMaking < Minitest::Test
     def test_that_prep_str_func_has_value
-        assert_equal("hello world", prepend_str("hello", "world"))
+        assert_equal("helloworld", prepend_str("hello", "world"))
     end
 end
     
--- methods.rb
+++ methods.rb
@@ -1,6 +1,5 @@
 def prepend_str(a, b)
-    a = "hello"
-    b = "world"
+    
     a + b
     
 end
