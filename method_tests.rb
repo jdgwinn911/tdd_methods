73c2d0b47a42252b7d6f92ff4f845b8604771470
Josh Gwinn <jdgwinn911@gmail.com>
2018-06-28 15:41:48 -0400

    another fail

--- method_tests.rb
+++ method_tests.rb
@@ -5,5 +5,8 @@ class MethodMaking < Minitest::Test
     def test_that_prep_str_func_has_value
         assert_equal("helloworld", prepend_str("hello", "world"))
     end
+
+    def test_that_array_has_elements
+        assert_equal(["a", "b", "c"], join_array([],"abc"))
+    end
 end
-    
--- methods.rb
+++ methods.rb
@@ -3,3 +3,9 @@ def prepend_str(a, b)
     a + b
     
 end
+
+def join_array(a1, b1)
+   a1 = []
+   b1 = "a b c"
+end
+   
