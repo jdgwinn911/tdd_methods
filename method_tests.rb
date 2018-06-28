8bb24cb70a16cfd64e96122e273e5c94a9f04014
Josh Gwinn <jdgwinn911@gmail.com>
2018-06-28 16:03:46 -0400

    fixed

--- method_tests.rb
+++ method_tests.rb
@@ -7,7 +7,7 @@ class MethodMaking < Minitest::Test
     end
 
     def test_that_array_has_elements
-        assert_equal(["a", "b", "c"], join_array([],"abc"))
+        assert_equal(["a", "b", "c", "a - b - c"], join_array([],"abc"))
     end
 
     # def test_that_array_returns_string_from_each_arr_element
