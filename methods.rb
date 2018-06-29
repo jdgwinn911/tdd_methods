7a550e6613a43563feceb4531d8a51c3d764ee53
Josh Gwinn <jdgwinn911@gmail.com>
2018-06-29 14:50:47 -0400

    last fix for join array

--- methods.rb
+++ methods.rb
@@ -7,11 +7,9 @@ end
 def join_array(arr, a) # a not so good way to do what array.join does lol
     arr = ["a", "b", "c"]
 
-    puts arr.at(0);puts arr.at(1); puts arr.at(2)  # using array.(index) to return an element at index
+    puts arr.at(0);puts arr.at(1); puts arr.at(2)  # using array. at (index) to return an element at index
     arr.at(0).to_s + arr.at(1).to_s + arr.at(2).to_s
-
 end
-
 
 # def flatten_hash(a2, b2)
 #     h = { "a" => 100, "b" => 200, ["c" => 300], "d" => 400}
